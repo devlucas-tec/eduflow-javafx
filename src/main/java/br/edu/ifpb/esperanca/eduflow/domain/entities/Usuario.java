edf32212538ed8e327504f559529e203fce4b7aa
Lucas Barbosa <barbosagraciano07@gmail.com>
2026-04-23 15:28:35 -0300

    feat: Adicionando dependências do jbcrypt para criptografia de senha e implementando Usuario

--- src/main/java/br/edu/ifpb/esperanca/eduflow/domain/entities/Usuario.java
+++ src/main/java/br/edu/ifpb/esperanca/eduflow/domain/entities/Usuario.java
@@ -11,6 +11,65 @@ public abstract class Usuario {
     private String matricula;
     protected Role role;
 
+    public Usuario(Long id, String nome, String email, String senhaHash, String matricula, Role role) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.senhaHash = senhaHash;
+        this.matricula = matricula;
+        this.role = role;
+    }
+
+    public Usuario() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenhaHash() {
+        return senhaHash;
+    }
+
+    public void setSenhaHash(String senhaHash) {
+        this.senhaHash = senhaHash;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
 
     public void editarPerfil() {
 
